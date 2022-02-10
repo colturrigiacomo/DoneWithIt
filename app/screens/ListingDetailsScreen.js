4f9adcb90b920587f29dd80bd3381f4864831cf5
giacomocolturri <giaki.coltu98@gmail.com>
2022-02-10 17:15:37 +0100

    Change ListingDetailScreem

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,5 +1,6 @@
 import { Image, View, StyleSheet } from "react-native";
 import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
 import colors from "../config/colors";
 
 function ListingDetailsScreen(props) {
@@ -9,6 +10,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket fot sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mosh.jpg")}
+            title="Giacomo Colturri"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -30,6 +38,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
