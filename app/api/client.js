55d6028d514f00e75b7745b0cc6c54632b3429ca
giacomocolturri <giaki.coltu98@gmail.com>
2022-03-22 15:50:02 +0100

    Some refactor

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,10 @@
 import { create } from "apisauce";
 import authStorage from "../auth/storage";
 import cache from "../utility/cache";
+import settings from "../config/settings";
 
 const apiClient = create({
-  baseURL: "http://192.168.1.81:9000/api",
+  baseURL: settings.apiUrl,
 });
 
 const get = apiClient.get;
